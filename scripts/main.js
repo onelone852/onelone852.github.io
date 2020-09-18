361010437f131f86c91461d1722e35fc193077a6
onelone852 <71179051+onelone852@users.noreply.github.com>
2020-09-18 15:17:14 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,13 @@ var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
 function setUserName() {
   let myName = prompt('輸入你的名字~');
+  if(！myName) {
+  } else {
+   setUserName();
     localStorage.setItem('name', myName);
     myHeading.innerHTML = '嗨! ' + myName;
   }
+}
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
